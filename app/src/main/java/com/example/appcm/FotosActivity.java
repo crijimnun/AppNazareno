ac865af8cec73fa9fa31c400143907255d7c5d0a
franbornos6 <fran_bornos6@hotmail.com>
2020-04-17 13:26:01 +0200

    Apartado Fotos

--- app/src/main/java/com/example/appcm/FotosActivity.java
+++ app/src/main/java/com/example/appcm/FotosActivity.java
@@ -4,18 +4,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FotosActivity extends AppCompatActivity {
 
+    GridView gridView;
+    String[] fruitNames = {"Apple","Orange","strawberry","Melon","Kiwi","Banana"};
+    int[] fruitImages = {R.drawable.apple,R.drawable.oranges,R.drawable.strawberry,R.drawable.watermelon,R.drawable.kiwi,R.drawable.banana};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fotos_activity);
 
+        gridView = findViewById(R.id.gridview);
+
+        CustomAdapter customAdapeter = new CustomAdapter();
+        gridView.setAdapter(customAdapeter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),GridItemActivity.class);
+                intent.putExtra("name",fruitNames[position]);
+                intent.putExtra("image",fruitImages[position]);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return fruitImages.length ;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.fila_data,null);
+
+            TextView name = view1.findViewById(R.id.fruits);
+            ImageView image = view1.findViewById(R.id.images);
+
+            name.setText(fruitNames[position]);
+            image.setImageResource(fruitImages[position]);
+
+            return view1;
+        }
+    }
+
+
+
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -24,7 +81,7 @@ public class FotosActivity extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        TextView lblMensaje= (TextView) findViewById(R.id.lblMensaje);
+        TextView lblMensaje= findViewById(R.id.lblMensaje);
         switch (item.getItemId()) {
             case R.id.noticias:
 //                lblMensaje.setText("¡Últimas Noticias!");
@@ -48,4 +105,6 @@ public class FotosActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
